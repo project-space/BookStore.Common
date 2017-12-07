a5ca34e450fbdcb0df240510f520085c56a67188
Elladiel <nastyazubastya1999@mail.ru>
2017-12-07 12:01:25 +0300

    apiClients

---
+++ src/BookStore.Common.BookServiceApiClient/GenreApiClient.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Common.BookServiceApiClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace BookStore.Common.BookServiceApiClient
+{
+    public class GenreApiClient
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public Genre GetGenre(string action)
+        {
+            
+            var response = httpClient.GetAsync($"http://localhost:55328/api/genres/{action}").Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<Genre>(json);
+        }
+    }
+}
---
+++ src/BookStore.Common.BookServiceApiClient/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Common.BookServiceApiClient.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+        public string CoverUrl { get; set; }
+        public int GenreId { get; set; }
+    }
+}
