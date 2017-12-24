0976dcbb52a3f61f4046b9c807f9eb08e5102064
Elladiel <nastyazubastya1999@mail.ru>
2017-12-24 21:21:08 +0300

    added PurchaseServiceClient

---
+++ src/ApiClients/BookStore.Common.PurchaseServiceClient/CartsClient.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Common.PurchaseServiceClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Common.PurchaseServiceClient
+{
+    public class CartsClient
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public Cart GetCart()
+        {
+            var response = httpClient.GetAsync($"http://localhost:50200//api/cart").Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<Cart>(json);
+        }
+    }
+}
