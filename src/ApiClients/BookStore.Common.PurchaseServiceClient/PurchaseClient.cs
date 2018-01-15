2b8dbfc12142ce81c41ef7a8efba39002cb97cd0
Elladiel <nastyazubastya1999@mail.ru>
2018-01-15 22:30:42 +0300

    added purchase api client

---
+++ src/ApiClients/BookStore.Common.PurchaseServiceClient/PurchaseClient.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Common.PurchaseServiceClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Common.PurchaseServiceClient
+{
+    public class PurchaseClient
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public void CreatePurchase(Order order)
+        {
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(order), Encoding.UTF8, "application/json");
+            httpClient.PostAsync($"http://localhost:50200/api/purchase/create", content).Wait();
+        }
+
+    }
+}
