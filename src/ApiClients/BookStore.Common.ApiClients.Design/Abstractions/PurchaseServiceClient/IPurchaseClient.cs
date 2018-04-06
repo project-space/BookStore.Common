80e15e990d298d82bb0e8a824c19018579990335
nasty <nasty@DESKTOP-5UP85BN>
2018-04-06 18:59:07 +0300

    refactored

--- src/ApiClients/BookStore.Common.ApiClients.Design/Abstractions/PurchaseServiceClient/IPurchaseClient.cs
+++ src/ApiClients/BookStore.Common.ApiClients.Design/Abstractions/PurchaseServiceClient/IPurchaseClient.cs
@@ -1,8 +1,4 @@
 ﻿using BookStore.Common.ApiClients.Design.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Refit;
 
