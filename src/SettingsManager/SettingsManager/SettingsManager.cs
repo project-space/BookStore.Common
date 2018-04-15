ddf50055225075f4b4d94fd24ce39dff6eba7c22
nasty <nasty@DESKTOP-5UP85BN>
2018-04-15 11:03:39 +0300

    created settings manager

---
+++ src/SettingsManager/SettingsManager/SettingsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsManager
+{
+    public class SettingsManager
+    {
+        public static String Read(String key)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filename: @"C:\Workspace C#\BookStore\settings\settings.xml");
+            XmlNodeList nodes = doc.GetElementsByTagName("setting");
+            foreach(XmlNode node in nodes){
+                if (node.Attributes["key"].Value == key)
+                {
+                    return node.Attributes["value"].Value;
+                }
+            }
+            return null;
+        }
+    }
+}
