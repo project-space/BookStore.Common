7775911a91a28e39c537048ddc4b8d499d2f0bbc
nasty <nasty@DESKTOP-5UP85BN>
2018-04-15 13:36:47 +0300

    try to fix this shit

--- src/SettingsManager/SettingsManager/SettingsManager.cs
+++ src/SettingsManager/SettingsManager/SettingsManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Xml;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +9,14 @@ namespace SettingsManager
 {
     public class SettingsManager
     {
-        public static String Read(String key)
+        public static String Read(Settings key)
         {
             XmlDocument doc = new XmlDocument();
             doc.Load(filename: @"C:\Workspace C#\BookStore\settings\settings.xml");
             XmlNodeList nodes = doc.GetElementsByTagName("setting");
-            foreach(XmlNode node in nodes){
-                if (node.Attributes["key"].Value == key)
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Attributes["key"].Value == key.ToString())
                 {
                     return node.Attributes["value"].Value;
                 }
